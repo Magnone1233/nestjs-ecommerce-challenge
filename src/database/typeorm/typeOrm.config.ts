950cec380032724e40709690ae15df51acf32bf1
David <david@MacBook-Air-de-David.local>
2026-03-25 19:44:32 -0300

    feat: changes for production

--- src/database/typeorm/typeOrm.config.ts
+++ src/database/typeorm/typeOrm.config.ts
@@ -7,16 +7,18 @@ const envFilePath: string = getEnvPath(
   resolve(__dirname, '../..', 'common/envs'),
 );
 config({ path: envFilePath });
+
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
-  host: process.env.DATABASE_HOST,
-  port: parseInt(process.env.DATABASE_PORT, 10),
-  database: process.env.DATABASE_NAME,
-  username: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  entities: [process.env.DATABASE_ENTITIES],
+
+  url: process.env.DATABASE_URL,
+
+  entities: ['dist/**/*.entity.js'],
+
   migrations: ['dist/database/migration/history/*.js'],
+
   logger: 'simple-console',
-  synchronize: false, // never use TRUE in production!
-  logging: true, // for debugging in dev Area only
+  logging: true,
+
+  synchronize: true,
 };
