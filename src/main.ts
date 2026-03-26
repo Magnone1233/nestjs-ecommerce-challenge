9698c6b45ec1b157b65e172d97a671e23471bdf4
David <david@MacBook-Air-de-David.local>
2026-03-25 21:11:52 -0300

    fix: problems

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  app.enableCors({
-    origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
-  });
-  const port = process.env.PORT || 3000;
-  await app.listen(port, '0.0.0.0');
+  app.enableCors();
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
